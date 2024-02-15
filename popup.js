19bdfec3d58956eeef14190b1395a72a4523aa28
staram <10952725+staram@users.noreply.github.com>
2024-02-15 16:14:38 +0000

    Add save to CSV function

--- popup.js
+++ popup.js
@@ -26,17 +26,16 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 });
 
 
-
-
-// SAVE PROMPT?
-// popup.js
+// SAVE PROMPT
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("save-button").addEventListener('click', saveTextToFile); 
+  document.getElementById("save-button-csv").addEventListener('click', saveToCSV); 
+  document.getElementById("save-button-txt").addEventListener('click', saveToTXT); 
 });
 
 
+//SAVE: txt
 
-function saveTextToFile() {
+function saveToTXT() {
   const textToSave = document.getElementById("page-text").value;
 
   if (!textToSave) {
@@ -49,11 +48,35 @@ function saveTextToFile() {
 
   chrome.downloads.download({
       url: url, 
-      filename: "SAVEFILE.txt",
+      filename: document.getElementById("page-title").value.replace(/[|&:;$%@"<>()+ ,]/g, "-").toLowerCase() + ".txt",
       saveAs: true
   });
 
-  
+  URL.revokeObjectURL(url);
+}
+
+
+
+//SAVE: csv
+function saveToCSV() {
+  const urlToSave = document.getElementById("page-url").value;
+  const titleToSave = document.getElementById("page-title").value;
+  const textToSave = document.getElementById("page-text").value;
+
+  if (!textToSave) {
+      alert("Please enter some text to save.");
+      return;
+  }
+
+  const csvContent = `"${urlToSave.replace(/"/g, '""')}","${titleToSave.replace(/"/g, '""')}","${textToSave.replace(/"/g, '""')}"\n`;
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+
+  chrome.downloads.download({
+      url: url,
+      filename: document.getElementById("page-title").value.replace(/[|&:;$%@"<>()+ ,]/g, "-").toLowerCase() + ".csv",
+      saveAs: true
+  });
 
   URL.revokeObjectURL(url);
 }
